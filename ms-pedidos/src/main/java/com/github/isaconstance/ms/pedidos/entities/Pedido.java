610610969d5629824bf0e2a85b12f8e01b935905
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 10:34:00 -0300

    feat: add entities, import.sql, application-test

---
+++ ms-pedidos/src/main/java/com/github/isaconstance/ms/pedidos/entities/Pedido.java
@@ -0,0 +1,48 @@
+package com.github.isaconstance.ms.pedidos.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    // @Column - define características da coluna no BD
+    // não é necessário o uso do "name"
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+    // ref -> @Column(unique = true, nullable = false, length = 11
+    @Column(nullable = false, length = 11)
+    private String cpf;
+    private LocalDate data;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    //valor calculado
+    private BigDecimal valorTotal;
+
+    //relacionamento
+    @OneToMany(mappedBy =  "pedido",
+            cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    public void calcularValorTotalDoPedido(){
+        this.valorTotal = this.itens.stream()
+                .map(i -> i.getPrecoUnitario()
+                        .multiply(BigDecimal.valueOf(i.getQuantidade())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+}
